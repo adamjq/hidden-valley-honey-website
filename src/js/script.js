c3e8e93faa5d56b90b797e3fc61f3a667135d876
Adam <adamjohnquigley@gmail.com>
2018-01-15 21:24:16 +1100

    Update map and add contact info box on top

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,17 @@
+/* Maps */
+
 var map = new GMaps({
       el: '#map',
       lat: -36.786,
-      lng: 175.108,
+      lng: 175.235,
       zoom: 11         
     });
+
+map.addMarker({
+  lat: -36.786,
+  lng: 175.108,
+  title: 'Waiheke Island',
+  infoWindow: {
+      content: '<p>Waiheke Island</p>'
+  }
+});
