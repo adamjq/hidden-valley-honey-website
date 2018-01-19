d72d4e0e5638e424be100e56c9d61deacd5c3680
Adam <adamjohnquigley@gmail.com>
2018-01-19 12:24:08 +1100

    Create sticky nav for larger devices

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,32 @@
-/* Maps */
-
-var map = new GMaps({
-      el: '#map',
-      lat: -36.786,
-      lng: 175.2,
-      zoom: 11         
+$(document).ready(function() {
+    
+    $('#products').waypoint(function(direction) {
+        
+        if (direction === "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px;'
     });
+    
+    
+    /* Maps */
+    var map = new GMaps({
+          el: '#map',
+          lat: -36.786,
+          lng: 175.2,
+          zoom: 11         
+        });
 
-map.addMarker({
-  lat: -36.786,
-  lng: 175.108,
-  title: 'Waiheke Island',
-  infoWindow: {
-      content: '<p>Waiheke Island</p>'
-  }
+    map.addMarker({
+      lat: -36.786,
+      lng: 175.108,
+      title: 'Waiheke Island',
+      infoWindow: {
+          content: '<p>Waiheke Island</p>'
+      }
+    });
+    
 });
